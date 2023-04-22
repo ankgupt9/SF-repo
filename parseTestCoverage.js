6b76ad8bbe3b58ae6f9a144487f27c951adb14c2
Ankur Gupta <agupta.ankur@gmail.com>
2023-04-22 13:05:58 -0400

    js file based change

---
+++ parseTestCoverage.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const readline = require('readline')
+
+async function extractTestCoverage(){
+
+    //by default we specify that all tests should run
+    const jsonString = fs.readFileSync('./test-results/coverage/coverage-summary.json')
+    var coverage = JSON.parse(jsonString)
+    var coveragePercent = coverage.total.lines.pct
+    await fs.promises.writeFile('./coverage-percentage.txt',coveragePercent);
+}
+
+extractTestCoverage();
