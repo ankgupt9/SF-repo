17245d64943baf0d7b518877d580d57df5e453aa
Ankur Gupta <agupta.ankur@gmail.com>
2023-04-22 16:42:58 -0400

    dsfds

--- parseTestCoverage.js
+++ parseTestCoverage.js
@@ -13,29 +13,18 @@ async function extractTestCoverage(){
     var coverage = JSON.parse(jsonString)
     var coveragePercent = coverage.total.lines.pct
     if (coveragePercent < 90 || coveragePercent == 'Unknown'){
-           const check = await octokit.rest.checks.create({
-                owner: github.context.repo.owner,
-                repo: github.context.repo.repo,
-                name: 'Readme Validator',
-                head_sha: github.context.sha,
-                status: 'completed',
-                conclusion: 'failure',
-                output: {
-                    title: 'README.md must start with a title',
-                    summary: 'Please use markdown syntax to create a title',
-                    annotations: [
-                        {
-                            path: 'README.md',
-                            start_line: 1,
-                            end_line: 1,
-                            annotation_level: 'failure',
-                            message: 'README.md must start with a header',
-                            start_column: 1,
-                            end_column: 1
-                        }
-                    ]
-                }
-            });
+            await octokit.request('POST /repos/{owner}/{repo}/statuses/{sha}', {
+              owner: github.context.repo.owner,
+              repo: github.context.repo.repo,
+              sha: github.context.sha,
+              state: 'failure',
+              target_url: 'https://example.com/build/status',
+              description: 'The build succeeded!',
+              context: 'continuous-integration/jenkins',
+              headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+              }
+            })
         throw "Low test coverage"
     }
 }
