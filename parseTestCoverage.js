6eff05dc793fdd74a811bb81708ef42928174895
Ankur Gupta <agupta.ankur@gmail.com>
2023-04-22 15:00:29 -0400

    ADD GITHUB STATUS

--- parseTestCoverage.js
+++ parseTestCoverage.js
@@ -9,7 +9,24 @@ async function extractTestCoverage(){
     if (coveragePercent < 90 || coveragePercent == 'Unknown'){
         throw "Low test coverage"
     }
-   // await fs.promises.writeFile('./coverage-percentage.txt',coveragePercent);
+    // Octokit.js
+// https://github.com/octokit/core.js#readme
+const octokit = new Octokit({
+    auth: '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-TOKEN'
+  })
+  
+  await octokit.request('POST /repos/{owner}/{repo}/statuses/{sha}', {
+    owner: 'OWNER',
+    repo: 'REPO',
+    sha: 'SHA',
+    state: 'success',
+    target_url: 'https://example.com/build/status',
+    description: 'The build succeeded!',
+    context: 'continuous-integration/jenkins',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
 }
 
 extractTestCoverage();
