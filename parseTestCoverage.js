386db6ea2e6dbc0a918d9424ba94f1242c293aee
Ankur Gupta <agupta.ankur@gmail.com>
2023-04-22 16:52:13 -0400

    hjjh

--- parseTestCoverage.js
+++ parseTestCoverage.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const readline = require('readline')
 const { Octokit } = require("@octokit/core");
-const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function extractTestCoverage(){
