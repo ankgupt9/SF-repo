a2c95c20ae0f871589e9bb789649335e5f5a13be
Ankur Gupta <agupta.ankur@gmail.com>
2023-04-22 22:25:02 -0400

    DFDFD

--- parseTestCoverage.js
+++ parseTestCoverage.js
@@ -16,7 +16,36 @@ async function extractTestCoverage(){
     var coverage = JSON.parse(jsonString)
     var coveragePercent = coverage.total.lines.pct
     if (coveragePercent < 90 || coveragePercent == 'Unknown'){
-            const check = await octokit.rest.checks.create({
+            
+      await octokit.request('POST /repos/{owner}/{repo}/check-runs', {
+        owner: 'OWNER',
+        repo: 'REPO',
+        name: 'mighty_readme',
+        head_sha: github.context.sha,
+        status: 'completed',
+        conclusion: 'failure',
+        output: {
+          title: 'Mighty Readme report',
+          summary: '',
+          text: '',
+          annotations: [
+            {
+                path: 'README.md',
+                start_line: 1,
+                end_line: 1,
+                annotation_level: 'failure',
+                message: 'README.md must start with a header',
+                start_column: 1,
+                end_column: 1
+            }
+          ]
+        },
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })
+
+    /*  const check = await octokit.rest.checks.create({
               owner: github.context.repo.owner,
               repo: github.context.repo.repo,
               name: 'Readme Validator',
@@ -38,7 +67,7 @@ async function extractTestCoverage(){
                       }
                   ]
               }
-          });
+          });*/
 
           core.setFailed('Low Code Coverage');
     }
