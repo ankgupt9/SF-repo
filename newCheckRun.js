6b0e280bbdbc1c65007ef7f6a24123381f1942ed
Ankur Gupta <agupta.ankur@gmail.com>
2023-04-30 17:23:08 -0400

    fdfd

--- newCheckRun.js
+++ newCheckRun.js
@@ -1,18 +1,17 @@
 const github = require('@actions/github');
 const fs = require('fs');
-const {App} = require('octokit');
-const privateKey = fs.readFileSync('./private-key.pem','UTF-8');
+const App = require('octokit');
 
 async function newCheckrun(){
 
   const repoName = github.context.repo.repo;
   const owner = github.context.repo.owner
-
+  var privateKey = fs.readFileSync('./private-key.pem','UTF-8');
   const app = new App({
-    appId: 322743,
+    appId: process.env.ANKUR_GIT_APP_ID,
     privateKey,
   });
-  const octokit = await app.getInstallationOctokit(36805855);
+  const octokit = await app.getInstallationOctokit(process.env.ANKUR_GIT_APP_INSTALL_ID);
   await octokit.request("GET /meta")
 }
 
