3be6b13eeadaa9cdd947623b9a37554047abb253
Ankur Gupta <agupta.ankur@gmail.com>
2023-04-30 17:51:54 -0400

    dfs

--- newCheckRun.js
+++ newCheckRun.js
@@ -10,13 +10,13 @@ async function newCheckrun(){
     privateKey,
   });
 
-  console.log('SHA' + github.context.sha)
+  console.log('SHA' + github.context.head_sha)
   const octokit = await app.getInstallationOctokit(process.env.ANKUR_GIT_APP_INSTALL_ID);
   await octokit.request('POST /repos/{owner}/{repo}/check-runs', {
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     name: 'mighty_readme',
-    head_sha: github.context.head_sha,
+    head_sha: github.event.pull_request.head.sha,
     status: 'completed',
     started_at: '2023-04-30T09:39:10Z',
     conclusion: 'success',
