19c65119fae775ba748a2b1cff0e4f2a6bd1856b
Ankur Gupta <agupta.ankur@gmail.com>
2023-04-30 16:48:21 -0400

    fdfd

--- newCheckRun.js
+++ newCheckRun.js
@@ -8,7 +8,7 @@ async function newCheckrun(){
   const owner = github.context.repo.owner
   var privateKey = fs.readFileSync('./private-key.pem','UTF-8')
   const app = new App({
-    appId: process.env.ANKUR_GIT_APP_ID,
+    appId: 322743,
     privateKey,
   });
   const octokit = await app.getInstallationOctokit(process.env.ANKUR_GIT_APP_INSTALL_ID);
